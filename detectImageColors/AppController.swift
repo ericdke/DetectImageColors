d85919e2b8ee72fb527a7db16cddd7dc4278d844
Eric Dejonckheere <eric@aya.io>
2015-04-05 00:39:53 +0200

    ok

--- detectImageColors/AppController.swift
+++ detectImageColors/AppController.swift
@@ -13,6 +13,16 @@ class AppController: NSObject {
     var colorTunes: ColorTunes?
     let pic = NSImage(named: "mic")
 
+    @IBOutlet weak var window: NSWindow!
+    @IBOutlet weak var label1: NSTextField!
+    @IBOutlet weak var label2: NSTextField!
+    @IBOutlet weak var label3: NSTextField!
+    @IBOutlet weak var label4: NSTextField!
+    @IBOutlet weak var image: NSImageView!
+
+
+
+
     override init() {
         colorTunes = ColorTunes(image: pic!, size: NSMakeSize(120.0, 120.0))
         super.init()
@@ -27,6 +37,12 @@ class AppController: NSObject {
         println(c.secondaryColorCandidate)
         println(c.detailColorCandidate)
         println(c.backgroundColorCandidate)
+//        let col1 = c.primaryColorCandidate!.colorUsingColorSpace(NSColorSpace.genericRGBColorSpace())
+        label1.textColor = c.primaryColorCandidate!
+        label2.textColor = c.secondaryColorCandidate!
+        label3.textColor = c.detailColorCandidate!
+        label4.textColor = c.backgroundColorCandidate!
+        image.image = pic!
     }
 
 }
