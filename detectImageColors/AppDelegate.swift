6939a8b9d0b3fb57782dfc949edd7b18cec54cf0
Eric Dejonckheere <eric@aya.io>
2015-04-04 03:30:44 +0200

    Initial Commit

---
+++ detectImageColors/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  detectImageColors
+//
+//  Created by ERIC DEJONCKHEERE on 04/04/2015.
+//  Copyright (c) 2015 Eric Dejonckheere. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
