9f2b6bf8b34361587698c1f3c125005b4a6c0950
Eric Dejonckheere <eric@aya.io>
2015-06-20 17:23:56 +0200

    updated playground with css values

--- DetectColorsPlayground.playground/Contents.swift
+++ DetectColorsPlayground.playground/Contents.swift
@@ -27,6 +27,17 @@ class DemoView: NSView {
     }
 }
 
+func makeTextField(frame: NSRect, color: NSColor, background: NSColor) -> NSTextField {
+    let tf = NSTextField(frame: frame)
+    tf.font = NSFont.systemFontOfSize(14)
+    tf.textColor = color
+    tf.backgroundColor = background
+    tf.stringValue = tf.textColor!.componentsCSS()!.css
+    tf.alignment = .CenterTextAlignment
+    tf.bordered = false
+    return tf
+}
+
 let mainView = NSView(frame: NSMakeRect(0, 0, 600, 600))
 let imageView = NSImageView(frame: NSMakeRect(150, 250, 300, 300))
 imageView.image = image
@@ -34,15 +45,28 @@ let primaryColorView = DemoView(frame: NSMakeRect(100, 50, 100, 150))
 let secondaryColorView = DemoView(frame: NSMakeRect(250, 50, 100, 150))
 let detailColorView = DemoView(frame: NSMakeRect(400, 50, 100, 150))
 let backgroundColorView = DemoView(frame: NSMakeRect(0, 0, 600, 600))
-primaryColorView.color = colorCandidates?.primary
-secondaryColorView.color = colorCandidates?.secondary
-detailColorView.color = colorCandidates?.detail
-backgroundColorView.color = colorCandidates?.background
+
+let primaryColor = colorCandidates!.primary!
+let secondaryColor = colorCandidates!.secondary!
+let detailColor = colorCandidates!.detail!
+let backgroundColor = colorCandidates!.background!
+
+primaryColorView.color = primaryColor
+secondaryColorView.color = secondaryColor
+detailColorView.color = detailColor
+backgroundColorView.color = backgroundColor
 
 mainView.addSubview(backgroundColorView)
 mainView.addSubview(imageView)
 mainView.addSubview(primaryColorView)
+mainView.addSubview(makeTextField(NSMakeRect(100, 20, 100, 20), primaryColor, backgroundColor))
+mainView.addSubview(makeTextField(NSMakeRect(100, 170, 100, 20), backgroundColor, primaryColor))
 mainView.addSubview(secondaryColorView)
+mainView.addSubview(makeTextField(NSMakeRect(250, 20, 100, 20), secondaryColor, backgroundColor))
+mainView.addSubview(makeTextField(NSMakeRect(250, 170, 100, 20), backgroundColor, secondaryColor))
 mainView.addSubview(detailColorView)
+mainView.addSubview(makeTextField(NSMakeRect(400, 20, 100, 20), detailColor, backgroundColor))
+mainView.addSubview(makeTextField(NSMakeRect(400, 170, 100, 20), backgroundColor, detailColor))
 
 XCPShowView("Colors", mainView)
+
